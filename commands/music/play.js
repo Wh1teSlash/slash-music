7f66d74f2456dcfe3f0c8022e4767869f80df8f0
WhiteSlashDev <github.eligibly235@passinbox.com>
2024-03-10 21:33:36 +0300

    fix #1

--- commands/music/play.js
+++ commands/music/play.js
@@ -1,141 +1,141 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js')
-const ms = require('ms')
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const ms = require("ms");
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('play')
-		.setDescription('Play a song.')
-		.addStringOption(option =>
-			option
-				.setName('query')
-				.setDescription('Paste query or link to the song')
-				.setRequired(true)
-		)
-		.toJSON(),
-	userPermissions: [],
-	botPermissions: [],
-	voiceOnly: true,
-	run: async (client, interaction) => {
-		try {
-			const query = interaction.options.getString('query')
+  data: new SlashCommandBuilder()
+    .setName("play")
+    .setDescription("Play a song.")
+    .addStringOption((option) =>
+      option
+        .setName("query")
+        .setDescription("Paste query or link to the song")
+        .setRequired(true)
+    )
+    .toJSON(),
+  userPermissions: [],
+  botPermissions: [],
+  voiceOnly: true,
+  run: async (client, interaction) => {
+    try {
+      const query = interaction.options.getString("query");
 
-			let player = await client.kazagumo.createPlayer({
-				guildId: interaction.guild.id,
-				textId: interaction.channel.id,
-				voiceId: interaction.member.voice.channel.id,
-				volume: 100,
-			})
+      let player = await client.kazagumo.createPlayer({
+        guildId: interaction.guild.id,
+        textId: interaction.channel.id,
+        voiceId: interaction.member.voice.channel.id,
+        volume: 100,
+      });
 
-			let result = await client.kazagumo.search(query, {
-				requester: interaction.user,
-				engine: 'spotify',
-			})
-			if (!result.tracks.length)
-				return interaction.reply({
-					content: 'No results found!',
-					ephemeral: true,
-				})
+      let result = await client.kazagumo.search(query, {
+        requester: interaction.user,
+        engine: "spotify",
+      });
+      if (!result.tracks.length)
+        return interaction.reply({
+          content: "No results found!",
+          ephemeral: true,
+        });
 
-			if (result.type === 'PLAYLIST')
-				for (let track of result.tracks) player.queue.add(track)
-			else player.queue.add(result.tracks[0])
+      if (result.type === "PLAYLIST")
+        for (let track of result.tracks) player.queue.add(track);
+      else player.queue.add(result.tracks[0]);
 
-			let source = ''
+      let source = "";
 
-			if (result.tracks[0].sourceName === 'spotify')
-				source = '<:spotify2:1101763629633765427>'
-			else if (result.tracks[0].sourceName === 'youtube')
-				source = '<:ytb:1101768846693634138>'
+      if (result.tracks[0].sourceName === "spotify")
+        source = "<:spotify2:1101763629633765427>";
+      else if (result.tracks[0].sourceName === "youtube")
+        source = "<:ytb:1101768846693634138>";
 
-			const trackEmbed = new EmbedBuilder()
-				.setColor('Blurple')
-				.setAuthor({
-					name: 'Song Added',
-					iconURL: interaction.member.displayAvatarURL({
-						size: 1024,
-						dynamic: true,
-					}),
-				})
-				.setFields([
-					{
-						name: '<:music:1100846700119195778> Track:',
-						value: `<:1_:1100848601808244789> **${result.tracks[0].author} - ${result.tracks[0].title}**`,
-					},
-					{
-						name: '<:friends:1100846696952508446> Requested by:',
-						value: `<:1_:1100848601808244789> ${result.tracks[0].requester}`,
-					},
-					{
-						name: '<:source:1101774575882227794> Source:',
-						value: `<:1_:1100848601808244789> ${source}`,
-					},
-				])
-				.setThumbnail(result.tracks[0].thumbnail)
-				.setFooter({
-					text: `Tracks in queue: ${player.queue.size} | Queue duration: ${ms(
-						player.queue.durationLength
-					)} | Volume: ${player.volume * 100}%`,
-				})
+      const trackEmbed = new EmbedBuilder()
+        .setColor("Blurple")
+        .setAuthor({
+          name: "Song Added",
+          iconURL: interaction.member.displayAvatarURL({
+            size: 1024,
+            dynamic: true,
+          }),
+        })
+        .setFields([
+          {
+            name: "<:music:1100846700119195778> Track:",
+            value: `<:1_:1100848601808244789> **${result.tracks[0].author} - ${result.tracks[0].title}**`,
+          },
+          {
+            name: "<:friends:1100846696952508446> Requested by:",
+            value: `<:1_:1100848601808244789> ${result.tracks[0].requester}`,
+          },
+          {
+            name: "<:source:1101774575882227794> Source:",
+            value: `<:1_:1100848601808244789> ${source}`,
+          },
+        ])
+        .setThumbnail(result.tracks[0].thumbnail)
+        .setFooter({
+          text: `Tracks in queue: ${player.queue.size} | Queue duration: ${ms(
+            player.queue.durationLength
+          )} | Volume: ${player.volume * 100}%`,
+        });
 
-			const playlistEmbed = new EmbedBuilder()
-				.setColor('Blurple')
-				.setAuthor({
-					name: 'Playlist Added',
-					iconURL: interaction.member.displayAvatarURL({
-						size: 1024,
-						dynamic: true,
-					}),
-				})
-				.setFields([
-					{
-						name: '<:music:1100846700119195778> Playlist:',
-						value: `<:1_:1100848601808244789> **${result.playlistName} | Added ${result.tracks.length} tracks**`,
-					},
-					{
-						name: '<:friends:1100846696952508446> Requested by:',
-						value: `<:1_:1100848601808244789> ${result.tracks[0].requester}`,
-					},
-					{
-						name: '<:source:1101774575882227794> Source:',
-						value: `<:1_:1100848601808244789> ${source}`,
-					},
-				])
-				.setThumbnail(result.tracks[0].thumbnail)
-				.setFooter({
-					text: `Tracks in queue: ${player.queue.size} | Queue duration: ${ms(
-						player.queue.durationLength
-					)} | Volume: ${player.volume * 100}%`,
-				})
+      const playlistEmbed = new EmbedBuilder()
+        .setColor("Blurple")
+        .setAuthor({
+          name: "Playlist Added",
+          iconURL: interaction.member.displayAvatarURL({
+            size: 1024,
+            dynamic: true,
+          }),
+        })
+        .setFields([
+          {
+            name: "<:music:1100846700119195778> Playlist:",
+            value: `<:1_:1100848601808244789> **${result.playlistName} | Added ${result.tracks.length} tracks**`,
+          },
+          {
+            name: "<:friends:1100846696952508446> Requested by:",
+            value: `<:1_:1100848601808244789> ${result.tracks[0].requester}`,
+          },
+          {
+            name: "<:source:1101774575882227794> Source:",
+            value: `<:1_:1100848601808244789> ${source}`,
+          },
+        ])
+        .setThumbnail(result.tracks[0].thumbnail)
+        .setFooter({
+          text: `Tracks in queue: ${player.queue.size} | Queue duration: ${ms(
+            player.queue.durationLength
+          )} | Volume: ${player.volume * 100}%`,
+        });
 
-			if (!player.playing && !player.paused) player.play()
+      if (!player.playing && !player.paused) player.play();
 
-			const message = player.data.get('message')
+      const message = player.data.get("message");
 
-			if (message) {
-				const embed = new EmbedBuilder(message.embeds[0]).setFooter({
-					text: `Tracks in queue: ${player.queue.size} | Queue duration: ${ms(
-						player.queue.durationLength
-					)} | Volume: ${player.volume * 100}%`,
-				})
-				message.edit({
-					embeds: [embed],
-				})
-			}
+      if (message) {
+        const embed = new EmbedBuilder(message.embeds[0]).setFooter({
+          text: `Tracks in queue: ${player.queue.size} | Queue duration: ${ms(
+            player.queue.durationLength
+          )} | Volume: ${player.volume * 100}%`,
+        });
+        message.edit({
+          embeds: [embed],
+        });
+      }
 
-			if (result.type === 'PLAYLIST')
-				return interaction.reply({
-					embeds: [playlistEmbed],
-					ephemeral: true,
-					files: [],
-				})
-			else
-				return interaction.reply({
-					embeds: [trackEmbed],
-					ephemeral: true,
-					files: [],
-				})
-		} catch (error) {
-			console.error(error)
-		}
-	},
-}
+      if (result.type === "PLAYLIST")
+        return interaction.reply({
+          embeds: [playlistEmbed],
+          ephemeral: true,
+          files: [],
+        });
+      else
+        return interaction.reply({
+          embeds: [trackEmbed],
+          ephemeral: true,
+          files: [],
+        });
+    } catch (error) {
+      console.error(error);
+    }
+  },
+};
--- kazagumo/events/playerStart.js
+++ kazagumo/events/playerStart.js
@@ -1,143 +1,121 @@
 const {
-	EmbedBuilder,
-	ActionRowBuilder,
-	ButtonBuilder,
-	ButtonStyle,
-} = require('discord.js')
-const ms = require('ms')
-const { Spotify } = require('canvafy')
-const sharp = require('sharp')
+  EmbedBuilder,
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+} = require("discord.js");
+const ms = require("ms");
+const { Spotify } = require("canvafy");
+const sharp = require("sharp");
 
 module.exports = {
-	name: 'playerStart',
-	async execute(client, player, track) {
-		let source = ''
-		let looped = ''
+  name: "playerStart",
+  async execute(client, player, track) {
+    let source = "";
+    let looped = "";
 
-		if (track.sourceName === 'spotify')
-			source = '<:spotify2:1101763629633765427>'
-		else if (track.sourceName === 'youtube')
-			source = '<:ytb:1101768846693634138>'
+    if (track.sourceName === "spotify")
+      source = "<:spotify2:1101763629633765427>";
+    else if (track.sourceName === "youtube")
+      source = "<:ytb:1101768846693634138>";
 
-		if (player.loop === 'none') looped = 'Playing'
-		else looped = `Looped: ${player.loop}`
+    if (player.loop === "none") looped = "Playing";
+    else looped = `Looped: ${player.loop}`;
 
-		let spotify = await new Spotify()
-			.setAuthor(track.author)
-			.setTitle(track.title)
-			.setImage(track.thumbnail)
-			.setTimestamp(1000, parseInt(track.length))
-			.setBlur(5)
-			.setOverlayOpacity(0.7)
-			.build()
+    const embed = new EmbedBuilder()
+      .setColor("Blurple")
+      .setAuthor({
+        name: looped,
+        iconURL: track.requester.displayAvatarURL({
+          size: 1024,
+          dynamic: true,
+        }),
+      })
+      .setFields([
+        {
+          name: "<:music:1100846700119195778> Track:",
+          value: `<:1_:1100848601808244789> **${track.author} - ${track.title}**`,
+        },
+        {
+          name: "<:friends:1100846696952508446> Requested by:",
+          value: `<:1_:1100848601808244789> ${track.requester}`,
+        },
+        {
+          name: "<:source:1101774575882227794> Source:",
+          value: `<:1_:1100848601808244789> ${source}`,
+        },
+      ])
+      .setThumbnail(track.thumbnail)
+      .setFooter({
+        text: `Tracks in queue: ${player.queue.size} | Queue duration: ${ms(
+          player.queue.durationLength
+        )} | Volume: ${player.volume * 100}%`,
+      });
 
-		let image = await sharp(spotify)
-			.resize(325, null, {
-				fit: 'inside',
-			})
-			.toBuffer()
+    const firstRow = new ActionRowBuilder().addComponents(
+      new ButtonBuilder()
+        .setCustomId("loopBtn")
+        .setEmoji("<:loop12:1100858090053709934>")
+        .setStyle(ButtonStyle.Secondary),
+      new ButtonBuilder()
+        .setCustomId("prevBtn")
+        .setEmoji("<:backforward:1111574632114442291>")
+        .setStyle(ButtonStyle.Secondary),
+      new ButtonBuilder()
+        .setCustomId("pauseBtn")
+        .setEmoji("<:stop:1100237060738138222>")
+        .setStyle(ButtonStyle.Secondary),
+      new ButtonBuilder()
+        .setCustomId("skipBtn")
+        .setEmoji("<:skip:1100237062306807860>")
+        .setStyle(ButtonStyle.Secondary),
+      new ButtonBuilder()
+        .setCustomId("shuffleBtn")
+        .setEmoji("<:shuffle:1111574753069768724>")
+        .setStyle(ButtonStyle.Secondary)
+    );
 
-		const response = await client.imgur.upload({
-			image: image,
-			type: 'stream',
-		})
+    const secondRow = new ActionRowBuilder().addComponents(
+      new ButtonBuilder()
+        .setCustomId("volumeBtn")
+        .setEmoji("<:volume:1100433632360923256>")
+        .setStyle(ButtonStyle.Secondary),
+      new ButtonBuilder()
+        .setCustomId("replayBtn")
+        .setEmoji("<:replay:1111578648055656502>")
+        .setStyle(ButtonStyle.Secondary),
+      new ButtonBuilder()
+        .setCustomId("stopBtn")
+        .setEmoji("<:incorrect:1087980863859462195>")
+        .setStyle(ButtonStyle.Danger),
+      new ButtonBuilder()
+        .setCustomId("infoBtn")
+        .setEmoji("<:info:1111576459342000210>")
+        .setStyle(ButtonStyle.Secondary),
+      new ButtonBuilder()
+        .setCustomId("requestBtn")
+        .setEmoji("<:friends:1100846696952508446>")
+        .setStyle(ButtonStyle.Secondary)
+    );
 
-		const card = response.data.link
+    const message = player.data.get("message");
 
-		const embed = new EmbedBuilder()
-			.setColor('Blurple')
-			.setAuthor({
-				name: looped,
-				iconURL: track.requester.displayAvatarURL({
-					size: 1024,
-					dynamic: true,
-				}),
-			})
-			.setFields([
-				{
-					name: '<:music:1100846700119195778> Track:',
-					value: `<:1_:1100848601808244789> **${track.author} - ${track.title}**`,
-				},
-				{
-					name: '<:friends:1100846696952508446> Requested by:',
-					value: `<:1_:1100848601808244789> ${track.requester}`,
-				},
-				{
-					name: '<:source:1101774575882227794> Source:',
-					value: `<:1_:1100848601808244789> ${source}`,
-				},
-			])
-			.setImage(card)
-			.setFooter({
-				text: `Tracks in queue: ${player.queue.size} | Queue duration: ${ms(
-					player.queue.durationLength
-				)} | Volume: ${player.volume * 100}%`,
-			})
-
-		const firstRow = new ActionRowBuilder().addComponents(
-			new ButtonBuilder()
-				.setCustomId('loopBtn')
-				.setEmoji('<:loop12:1100858090053709934>')
-				.setStyle(ButtonStyle.Secondary),
-			new ButtonBuilder()
-				.setCustomId('prevBtn')
-				.setEmoji('<:backforward:1111574632114442291>')
-				.setStyle(ButtonStyle.Secondary),
-			new ButtonBuilder()
-				.setCustomId('pauseBtn')
-				.setEmoji('<:stop:1100237060738138222>')
-				.setStyle(ButtonStyle.Secondary),
-			new ButtonBuilder()
-				.setCustomId('skipBtn')
-				.setEmoji('<:skip:1100237062306807860>')
-				.setStyle(ButtonStyle.Secondary),
-			new ButtonBuilder()
-				.setCustomId('shuffleBtn')
-				.setEmoji('<:shuffle:1111574753069768724>')
-				.setStyle(ButtonStyle.Secondary)
-		)
-
-		const secondRow = new ActionRowBuilder().addComponents(
-			new ButtonBuilder()
-				.setCustomId('volumeBtn')
-				.setEmoji('<:volume:1100433632360923256>')
-				.setStyle(ButtonStyle.Secondary),
-			new ButtonBuilder()
-				.setCustomId('replayBtn')
-				.setEmoji('<:replay:1111578648055656502>')
-				.setStyle(ButtonStyle.Secondary),
-			new ButtonBuilder()
-				.setCustomId('stopBtn')
-				.setEmoji('<:incorrect:1087980863859462195>')
-				.setStyle(ButtonStyle.Danger),
-			new ButtonBuilder()
-				.setCustomId('infoBtn')
-				.setEmoji('<:info:1111576459342000210>')
-				.setStyle(ButtonStyle.Secondary),
-			new ButtonBuilder()
-				.setCustomId('requestBtn')
-				.setEmoji('<:friends:1100846696952508446>')
-				.setStyle(ButtonStyle.Secondary)
-		)
-
-		const message = player.data.get('message')
-
-		if (message) {
-			message
-				.edit({
-					embeds: [embed],
-				})
-				.then(x => player.data.set('message', x))
-			let spotify = null
-		} else {
-			client.channels.cache
-				.get(player.textId)
-				?.send({
-					embeds: [embed],
-					components: [firstRow, secondRow],
-				})
-				.then(x => player.data.set('message', x))
-			let image = null
-		}
-	},
-}
+    if (message) {
+      message
+        .edit({
+          embeds: [embed],
+        })
+        .then((x) => player.data.set("message", x));
+      let spotify = null;
+    } else {
+      client.channels.cache
+        .get(player.textId)
+        ?.send({
+          embeds: [embed],
+          components: [firstRow, secondRow],
+        })
+        .then((x) => player.data.set("message", x));
+      let image = null;
+    }
+  },
+};
